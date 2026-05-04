1799180765d4b74688216ea1e8a9873df2b18d86
ally yurukova <ayurukov@uci.edu>
2026-05-03 20:49:17 -0700

    Updated navbar to match out sitemap

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,10 +4,14 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav>
+      <h2>Wildfire Tracker</h2>
       <Link to="/">Home</Link>
-      <Link to="/map">Map</Link>
-      <Link to="/alerts">Alerts</Link>
-      <Link to="/about">About</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/dashboard">/Dashboard</Link>
+      <Link to="/help">Help Resources</Link>
+      <Link to="/profile">Profile</Link>
+      <Link to="/offline">Offline</Link>
+      <Link to="/settings">Settings</Link>
     </nav>
   );
 }
