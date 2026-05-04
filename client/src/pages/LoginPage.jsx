ae85fe8cc74c85c256429d10442657404c853ef5
ally yurukova <ayurukov@uci.edu>
2026-05-03 22:26:33 -0700

    fixed the pages to match sitemap and di the login page

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,4 +1,11 @@
-// src/pages/Map.jsx
-export default function Map() {
-  return <h1>Wildfire Map</h1>;
+export default function LoginPage() {
+  return (
+    <div className="page">
+      <h1>Login Page</h1>
+      <div className="dashboard-links">
+        <button>Email Login</button>
+        <button>Google Login</button>
+      </div>
+    </div>
+      );
 }
